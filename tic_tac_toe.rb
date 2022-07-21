f8d6044b8cbe1454a41e092be5d0a11a62575aae
Matthew <matthewgonzalez2324@gmail.com>
2022-07-21 03:33:09 -0700

    Create check_for_winner method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -69,23 +69,82 @@ class TicTacToe
     add_player(isPlayer2=true)
     create_grid
     no_winner = true
+    turn_count = 0
     loop do
       # player1's turn 
       puts "#{@players[0].name}, please select a cell 1-9."
       cell_player1 = gets.chomp().to_i
       place_char(@players[0].character, cell_player1)
-      # logic to check for winner
-      if (@@grid[0].all? {|c| c == "X" || c == "O"} || @@grid[1].all? {|c| c == "X" || c == "O"} || @@grid[2].all? {|c| c == "X" || c == "O"})
-        no_winner = false
-        puts "There was a winner"
+      turn_count += 1
+      # check for winner if at least 5 turns have been played
+      if turn_count >= 5
+        no_winner = check_for_winner
+        break if no_winner == false
       end
       # player2's turn
       puts "#{@players[1].name}, please select a cell 1-9."
       cell_player2 = gets.chomp().to_i
       place_char(@players[1].character, cell_player2)
+      turn_count += 1
+      if turn_count >= 5
+        no_winner = check_for_winner
+        break if no_winner == false
+      end
 
       break if no_winner == false
     end
+  end
+
+  def check_for_winner
+    no_winner = true
+    diagonal_win = [@@grid[0].first, @@grid[1][1], @@grid[2].last]
+    backwards_diagonal_win = [@@grid[0].last, @@grid[1][1], @@grid[2].first]
+    left_vertical_win = [@@grid[0].first, @@grid[1].first, @@grid[2].first]
+    center_vertical_win = [@@grid[0][1], @@grid[1][1], @@grid[2][1]]
+    right_vertical_win = [@@grid[0].last, @@grid[1].last, @@grid[2].last]
+    # check if player 1 won
+    if (@@grid[0].all? {|c| c == @players[0].character} || @@grid[1].all? {|c| c == @players[0].character} || @@grid[2].all? {|c| c == @players[0].character})
+      puts "#{@players[0].name} won!"
+      no_winner = false
+    elsif diagonal_win.all? {|c| c != "_"} && diagonal_win.all? {|c| c == @players[0].character}
+      puts "#{@players[0].name} won diagonally!"
+      no_winner = false
+    elsif backwards_diagonal_win.all? {|c| c != "_"} && backwards_diagonal_win.all? {|c| c == @players[0].character}
+      puts "#{@players[0].name} won backwards diagonally!"
+      no_winner = false
+    elsif left_vertical_win.all? {|c| c!= "_"} && left_vertical_win.all? {|c| c == @players[0].character}
+      puts "#{@players[0].name} won vertically!"
+      no_winner = false
+    elsif center_vertical_win.all? {|c| c!= "_"} && center_vertical_win.all? {|c| c == @players[0].character}
+      puts "#{@players[0].name} won vertically!"
+      no_winner = false
+    elsif right_vertical_win.all? {|c| c!= "_"} && right_vertical_win.all? {|c| c == @players[0].character}
+      puts "#{@players[0].name} won vertically!"
+      no_winner = false
+    end
+
+    # check if player 2 won
+    if (@@grid[0].all? {|c| c == @players[1].character} || @@grid[1].all? {|c| c == @players[1].character} || @@grid[2].all? {|c| c == @players[1].character})
+      puts "#{@players[1].name} won!"
+      no_winner = false
+    elsif diagonal_win.all? {|c| c != "_"} && diagonal_win.all? {|c| c == @players[1].character}
+      puts "#{@players[1].name} won diagonally!"
+      no_winner = false
+    elsif backwards_diagonal_win.all? {|c| c != "_"} && backwards_diagonal_win.all? {|c| c == @players[1].character}
+      puts "#{@players[1].name} won backwards diagonally!"
+      no_winner = false
+    elsif left_vertical_win.all? {|c| c!= "_"} && left_vertical_win.all? {|c| c == @players[1].character}
+      puts "#{@players[1].name} won vertically!"
+      no_winner = false
+    elsif center_vertical_win.all? {|c| c!= "_"} && center_vertical_win.all? {|c| c == @players[1].character}
+      puts "#{@players[1].name} won vertically!"
+      no_winner = false
+    elsif right_vertical_win.all? {|c| c!= "_"} && right_vertical_win.all? {|c| c == @players[1].character}
+      puts "#{@players[1].name} won vertically!"
+      no_winner = false
+    end
+
+    return no_winner
   end
 
 end
